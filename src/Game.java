c832a11d877110be894f0d81e92d25e62d288b23
Andreas Selguson <andreas.selguson@saabgroup.com>
2023-10-09 13:33:53 +0200

    Error handling

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,11 @@
+import jdk.jshell.spi.ExecutionControlProvider;
+
 import javax.swing.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 
 public class Game {
@@ -14,17 +17,14 @@ public class Game {
     private Board board;
     private int cnt;
     private ArrayList<Integer> positions;
+
     public Game() {
     }
 
 
-    public void play () {
+    public void play() {
         Scanner sc = new Scanner(System.in);
         boolean play = true;
-
-
-
-
 
         //Get player names
         System.out.println("Ange namn för spelare 1: ");
@@ -33,7 +33,7 @@ public class Game {
 
         System.out.println("Vill du spela mot datorn? (J/N)");
         String answer = sc.nextLine();
-        if(answer.equals("J")) {
+        if (answer.equals("J")) {
             //Skapa datorspelare
             players.add(new Player("Dator", "O"));
             sc.nextLine();
@@ -47,48 +47,53 @@ public class Game {
             board = new Board(size);
             positions = new ArrayList<>();
             cnt = 0;
-            for(int i = 1; i <= size; i++){
+            for (int i = 1; i <= size; i++) {
                 positions.add(i);
             }
+
             while (!gameEnds) {
-                cnt++;
-                if (cnt > 9) {
+
+                if (cnt == 9) {
                     System.out.println("Spelet slutade oavgjort!");
                     break;
                 }
+
+                //Print gameboard
                 System.out.println(" " + board.getValues().get(0) + " | " + board.getValues().get(1) + " | " + board.getValues().get(2) + " ");
                 System.out.println("---+---+---");
                 System.out.println(" " + board.getValues().get(3) + " | " + board.getValues().get(4) + " | " + board.getValues().get(5) + " ");
                 System.out.println("---+---+---");
                 System.out.println(" " + board.getValues().get(6) + " | " + board.getValues().get(7) + " | " + board.getValues().get(8) + " ");
-                if(players.get(activePlayer).getName().equals("Dator")){
+
+                //If computerplayer random a valid int and play it
+                if (players.get(activePlayer).getName().equals("Dator")) {
                     Random rnd = new Random();
                     position = positions.get(rnd.nextInt(positions.size()));
-                    positions.remove(Integer.valueOf(position));
-                }
-                else {
                     System.out.println("Spelare " + players.get(activePlayer).getName() + ", vilken ruta (1-9)?");
-                    position = sc.nextInt();
-                }
-                //Instead of a huge Swtichstatement i check if the input is between 1-9 and play if so.
-                Boolean validInput = false;
-                while (!validInput) {
-                    int digit = position % 10;
-                    if (digit < 1 || digit > 9) {
-                        validInput = false;
-                        break;
+                    System.out.println(position);
+                } else {
+                    //If human player
+                    System.out.println("Spelare " + players.get(activePlayer).getName() + ", vilken ruta (1-9)?");
+                    try {
+                        position = sc.nextInt();
+                        sc.nextLine();
+                    } catch (Exception ex) {
+                        position = 0;
+                        System.out.println("Du angav ett ogiltigt värde, error: " + ex.getMessage());
+                        sc.nextLine();
                     }
-                    validInput = true;
-                    positions.remove(Integer.valueOf(position));
                 }
 
                 //If position is correct, do the move
-                if (validInput)
+                if (positions.contains(position)) {
+                    positions.remove(Integer.valueOf(position));
                     play2();
+                    cnt++;
+                }
                 else {
                     //If incorrect position, print and exit
                     System.out.println("Försök igen!");
-                    break;
+                    //break;
                 }
             }
 
@@ -96,17 +101,20 @@ public class Game {
             System.out.println("Spela ett varv till? (J/N)");
             sc.nextLine();
             answer = sc.nextLine();
-            if(!answer.equals("J")) {
+
+            if (!answer.equals("J")) {
                 play = false;
-
-
             }
+
             gameEnds = false;
+
         } while (play);
 
+        //Print result of the game
         System.out.println("Spelare: " + players.get(0).getName() + " vann: " + players.get(0).getWins() + " gånger! Grattis!");
         System.out.println("Spelare: " + players.get(1).getName() + " vann: " + players.get(1).getWins() + " gånger! Grattis!");
     }
+
     public boolean winner(String x, String y, String z, String player) {
         //Compare different values in the positions to check if theres a winner
         if (x.equals(" "))
@@ -129,8 +137,8 @@ public class Game {
     public boolean play(ArrayList<String> values, String player) {
         boolean winner = false;
         //Call method to check if the marker is the same in a row horizontal
-        for(int x = 0; x < 3; x++) {
-            winner = winner(values.get(x), values.get(x+1), values.get(x+2), player);
+        for (int x = 0; x < 3; x++) {
+            winner = winner(values.get(x), values.get(x + 1), values.get(x + 2), player);
             if (winner)
                 return true;
             x++;
@@ -138,8 +146,8 @@ public class Game {
         }
 
         //Call method to check if the marker is the same in a row vertical
-        for(int x = 0; x < 3; x++) {
-            winner = winner(values.get(x), values.get(x+3), values.get(x+6), player);
+        for (int x = 0; x < 3; x++) {
+            winner = winner(values.get(x), values.get(x + 3), values.get(x + 6), player);
             if (winner)
                 return true;
         }
@@ -158,17 +166,18 @@ public class Game {
 
     }
 
-    public void flipPlayer (){
+    public void flipPlayer() {
         //Flips so next player is the opposite after every turn of the game
         if (activePlayer == 0)
             activePlayer = 1;
         else
             activePlayer = 0;
     }
-    public void play2 (){
+
+    public void play2() {
         //Check if position is available and if so write the correct marker to that position
-        if (board.getValues().get(position-1).equals(" ")) {
-            board.getValues().set(position-1, players.get(activePlayer).getMarker());
+        if (board.getValues().get(position - 1).equals(" ")) {
+            board.getValues().set(position - 1, players.get(activePlayer).getMarker());
 
             //Call method to check if we have a winner and the game should end.
             gameEnds = play(board.getValues(), players.get(activePlayer).getMarker());
